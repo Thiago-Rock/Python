2fa94d537de886fad1b1457ace1d81d5be957914
Thiago Rocha <108189713+Thiago-Rock@users.noreply.github.com>
2025-03-23 16:41:38 -0300

    Implementação do código

---
+++ Lista Prof. Oswaldo/exercício1_Lista1.py
@@ -0,0 +1,14 @@
+'''
+1. A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as dimensões de um terreno e depois exibir a área do terreno. 
+'''
+print("Programa para dizer a área do terreno")
+print("#####################################")
+print("\n")
+
+comprimento = float(input("Entre com o comprimento do terrno :"))
+largura = float(input("Entre com a largura do terrno :"))
+
+area = comprimento*largura
+
+
+print("A área do terreno é ", area, " m²")
