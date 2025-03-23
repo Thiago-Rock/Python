56b60312557251ce8f55575873f4a6ad31bea60f
Thiago Rocha <108189713+Thiago-Rock@users.noreply.github.com>
2025-03-23 12:58:20 -0300

    Meu primeiro envio

---
+++ 25.03.14/exercício0.py
@@ -0,0 +1,9 @@
+# Entre com dois números e mostre a soma deles.
+
+#print("Digite o primeiro número: ")
+N1 = float(input("Digite o primeiro número: "))
+#print("\nDigite o segundo número: ")
+N2 = float(input("\nDigite o segundo número: "))
+
+soma = N1 + N2
+print("A soma dos números é: ", soma)
