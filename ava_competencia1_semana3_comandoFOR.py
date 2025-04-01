6fdf6e35ebc3dca988df38455a64e74de7666323
Thiago Rocha <108189713+Thiago-Rock@users.noreply.github.com>
2025-04-01 10:37:46 -0300

    Desafio: Uso do comando for pra fazer a média e a variancia do primeiro e do segundo elemento de cada linha

---
+++ ava_competencia1_semana3_comandoFOR.py
@@ -0,0 +1,46 @@
+temps = [[20, 19.5],[20, 19.7],[20, 20.2],[20,20.1],[21,19.9],[21,20.7],[21,21.2],[21,21.5],[25,25.5],[25,24.7],[25,25.2],[25,24.1]]
+media_PV = 0
+media_SP = 0
+desvio_PV = 0
+desvio_SP = 0
+n = 0
+controle=0
+for linha in temps:
+    for PV in linha:
+        if controle % 2 == 0:
+            media_PV += PV
+            controle+=1
+            n+=1
+            
+            
+        else:
+            media_SP += PV
+            controle+=1
+
+media_PV = media_PV/n
+media_SP = media_SP/n 
+
+controle=0
+for linha in temps:
+    for j in linha:
+        if controle % 2 == 0:
+            desvio_PV += pow(j - media_PV,2)
+            controle+=1
+            
+            
+            
+        else:
+            desvio_SP += pow(j - media_SP,2)
+            controle+=1
+
+desvio_PV = pow(desvio_PV/n,0.5)
+desvio_SP = pow(desvio_SP/n,0.5)
+
+
+
+print(media_PV)
+print(media_SP)
+print("n = ", n)
+print(desvio_PV)
+print(desvio_SP)
+       
