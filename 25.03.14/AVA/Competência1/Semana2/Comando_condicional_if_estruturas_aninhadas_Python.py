aa3db02389331b97be3cba4616d766d12ec67680
Thiago Rocha <108189713+Thiago-Rock@users.noreply.github.com>
2025-04-02 07:02:33 -0300

    delete

--- 25.03.14/AVA/Competência1/Semana2/Comando_condicional_if_estruturas_aninhadas_Python.py
+++
@@ -1,9 +0,0 @@
-temp = float(input('Qual a temperatura? '))
-if temp < 7:
-    print("Congelando!")
-elif temp < 10:
-    print("Frio!")
-elif temp < 26:
-    print("Ótimo!")
-else:
-    print("Muito quente!")
